2772e854d986a802caab75872944ec1de690de58
Your Name <you@example.com>
2020-06-22 20:12:27 -0500

    hello world program

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,8 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          
+          Hello World
+
         </a>
       </header>
     </div>
