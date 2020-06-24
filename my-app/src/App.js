0d1b4ae75b2dccf973642361c1d3b2678c0142ba
Your Name <you@example.com>
2020-06-23 19:56:39 -0500

    Class Component Basics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Greet';
+import Welcome from './Welcome';
 
 function App() {
   return (
    <div className="App">
       <Greet>Saideep</Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
---
+++ my-app/src/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+// Simple  Greet in Class Component
+class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Class Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Welcome
